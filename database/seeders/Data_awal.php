34434ce5ef04856be20aa1679d24deb3b602e45b
xeizer <keiser.form@gmail.com>
2023-02-03 10:20:07 +0700

    selesai login & logout

--- database/seeders/Data_awal.php
+++ database/seeders/Data_awal.php
@@ -15,6 +15,22 @@ class Data_awal extends Seeder
      */
     public function run()
     {
-        $admin = new User();
+        $admin              = new User();
+        $admin->nik         = '617101072000';
+        $admin->nama        = 'Dior jordan';
+        $admin->email       = 'Diorgemink@sosis.me';
+        $admin->password    = bcrypt('bangmessi');
+        $admin->telp        = '0856787765';
+        $admin->level       =  'admin';
+        $admin->save();
+
+        $petugas              = new User();
+        $petugas->nik         = '61720205623';
+        $petugas->nama        = 'Dior Major';
+        $petugas->email       = 'Majorgemink@sosis.me';
+        $petugas->password    = bcrypt('bangdodo');
+        $petugas->telp        = '085845453436';
+        $petugas->level       =  'petugas';
+        $petugas->save();
     }
 }
